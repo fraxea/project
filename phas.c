dca5f009034cc448a08580023aa6cf2029fcee30
fraxea <moh788672@gmail.com>
2023-01-07 05:32:51 +0330

    multiplication

--- phas.c
+++ phas.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#define n 10
+
+void printBoard() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%3i ", i * j);
+		}
+		printf("\n");
+	}
 
 int main() {
+	printf("Product table\n,");
 	printf("Phase 1\n");
+	printf("See:\n");
+	printBoard();
 	return 0;
 }
